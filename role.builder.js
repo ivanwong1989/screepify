1ce192c43313b6d14279dde3d690e3ab90909abe
ivanwong1989 <ivanwong1989@gmail.com>
2026-01-29 22:25:47 +0800

    haha

--- role.builder.js
+++ role.builder.js
@@ -21,8 +21,8 @@ var roleBuilder = {
             }
 	    }
 	    else {
-	        var sources = creep.room.find(FIND_SOURCES);
-            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+	        var sources = creep.room.find(FIND_MY_SPAWNS);
+            if(creep.withdraw(sources[0],RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
             }
 	    }
