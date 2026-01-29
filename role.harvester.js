f8dc7bca3b3e8f7a512e290364967d68093c5893
ivanwong1989 <ivanwong1989@gmail.com>
2026-01-29 20:54:16 +0800

    hola

--- role.harvester.js
+++ role.harvester.js
@@ -3,14 +3,22 @@ var roleHarvester = {
     /** @param {Creep} creep **/
     run: function(creep) {
 	    if(creep.store.getFreeCapacity() > 0) {
-            var sources = creep.room.find(FIND_SOURCES);
-            console.log(sources);
-            if (creep.memory.random_source_target == "NA") {
-                creep.memory.random_source_target = sources[Math.floor(Math.random() * sources.length)];
+            if (creep.memory.random_source_target == "NA" || !creep.memory.random_source_target) {
+                var sources = creep.room.find(FIND_SOURCES);
+                creep.memory.random_source_target_id = sources[Math.floor(Math.random() * sources.length)].id;
             }
-            if(creep.harvest(Game.getObjectById(creep.memory.random_source_target.id)) == ERR_NOT_IN_RANGE) {
-                creep.say('🔄 harvest');
-                creep.moveTo(Game.getObjectById(creep.memory.random_source_target.id));
+
+            
+            var source = Game.getObjectById(creep.memory.random_source_target_id);
+            if(source) {
+                // since chosen a source, let's see if the source is dangerous around it. 
+                var enemies = creep.memory.random_source_target.findInRange(FIND_HOSTILE_CREEPS,5);
+                if(enemies.length == 0) {
+                    if(creep.harvest(source) == ERR_NOT_IN_RANGE) {
+                        creep.say('🔄 harvest');
+                        creep.moveTo(source);
+                    }
+                }
             }
         }
         else {
