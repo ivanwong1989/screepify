075a376e3fb0edea4108a501208d3d8156d28adb
ivanwong1989 <ivanwong1989@gmail.com>
2026-01-30 00:07:50 +0800

    asd

--- role.harvester.js
+++ role.harvester.js
@@ -49,8 +49,16 @@ var roleHarvester = {
             // free the random target source memory and also harvesting wip status
             creep.memory.random_source_target_id = "NA";
             creep.memory.harvesting_wip = 0;
-            if(creep.transfer(Game.spawns['Spawn1'], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(Game.spawns['Spawn1']);
+            var targets = creep.room.find(FIND_STRUCTURES, {
+            filter: (structure) => {
+                return (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN || structure.structureType == STRUCTURE_TOWER) &&
+                    structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
+            }
+            });
+            if(targets.length > 0) {
+                if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
+                }
             }
         }
 	}
