9418afa1d01b76035bd873fd0f07a03c2bfbac9b
ivanwong1989 <ivanwong1989@gmail.com>
2026-01-29 22:10:31 +0800

    fix

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 var roleHarvester = require('role.harvester');
 var roleUpgrader = require('role.upgrader');
 var roleBuilder = require('role.builder');
-var roleTower = require('role.tower')
+var roleTower = require('role.tower');
 
 module.exports.loop = function () {
 
